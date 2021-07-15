f8f54b4053e2febe518f4c9b7539db32d96e9137
Arcywojak <maciej778@op.pl>
2021-07-15 14:50:51 +0200

    add simple configuration of webpack

---
+++ webpack.prod.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const merge = require('webpack-merge')
+
+module.exports = merge(common, {
+  mode: "production",
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),  
+  },
+});
